2216d57d7c92a79e5a575fdb92d98486947ea6a9
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-23 19:07:19 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -56,15 +56,6 @@ export const App = () => {
                   >
                     -
                   </button>
-                ) : selectedGood === '' ? (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => setValue(good)}
-                  >
-                    +
-                  </button>
                 ) : (
                   <button
                     data-cy="AddButton"
